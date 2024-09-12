b7b9f399fc832c248967ecac28e32c981201a73e
Lauri Leukkunen <lauri.leukkunen@gmail.com>
2024-09-12 08:02:01 +0300

    Bump version number
    Signed-off-by: Lauri Leukkunen <lauri.leukkunen@gmail.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="eigengen",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
