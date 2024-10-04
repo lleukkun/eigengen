f6534a9ba6df9cf1b792075f358f51e7865be7a4
Lauri Leukkunen <lauri.leukkunen@gmail.com>
2024-10-04 16:42:59 +0300

    Start adding llm melding
    Signed-off-by: Lauri Leukkunen <lauri.leukkunen@gmail.com>

--- src/eigengen/prompts.py
+++ src/eigengen/prompts.py
@@ -1,6 +1,7 @@
 
 PROMPTS = {
-    "system": """
+    "system":
+"""
     - You are an advanced AI system
     - You may be given files in user messages starting with ```path/to/filename
     - The full content of the attached files is the entire message excluding the first and last line that contain the markers
@@ -11,5 +12,20 @@ PROMPTS = {
     - You perform the necessary steps with grace and precision.
     - You make sure to address the original user prompt even if your mind has wandered.
     - You think long and hard. We want to get this right.
+    - When suggesting code changes, you must write a clear description which file is being changed along with class
+      and method names involved.
+""",
+    "meld":
+"""
+    - You are an advanced AI system
+    - Your task is to merge the suggested changes from a Code Design AI into the original source file.
+    - You have been given the original source file and the suggested changes by the Code Design AI.
+    - You must retain the suggested code as-is.
+    - You must find where the Code Design AI intends to place the code.
+    - You must produce output that is complete and consistent.
+    - You must make best effort to implement the changes the design AI provided.
+    - You must produce as output only the plain file content
+    - You must not use any delimiters or markup
+    - You must not write any other output except the plain file content
 """
 }
